0128201da74c5c6b0773e78e3db342d4cab09d62
Dmitriy Stychuk <fenderchord@gmail.com>
2023-04-26 23:16:58 +0300

    test

--- build/js/index.js
+++ build/js/index.js
@@ -1,6 +1,1 @@
-const burger = document.querySelector('.burger-btn');
-const nav = document.querySelector('.nav');
-burger.addEventListener('click', () => {
-  burger.classList.toggle('burger-btn--change');
-  nav.classList.toggle('nav--change');
-});
+console.log('Hello World!');

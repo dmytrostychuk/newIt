60b5d866a7b9a45b98ac474f06f6552368ae377b
Dmitriy Stychuk <fenderchord@gmail.com>
2023-04-26 23:14:49 +0300

    add new tags

--- build/js/index.js
+++ build/js/index.js
@@ -1,16 +1,6 @@
-let burgerBtn = document.querySelector('.burger-btn');
-let menuNav = document.querySelector('.nav');
-
-if (burgerBtn) {
-  burgerBtn.addEventListener('click', function (e) {
-    // document.body.classList.toggle('lock');
-    if (menuNav.classList.contains('nav__active')) {
-      menuNav.classList.remove('nav__active');
-      menuNav.classList.add('nav__close');
-    } else {
-      menuNav.classList.remove('nav__close');
-      menuNav.classList.add('nav__active');
-    }
-    burgerBtn.classList.toggle('burger-btn-active');
-  });
-}
+const burger = document.querySelector('.burger-btn');
+const nav = document.querySelector('.nav');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger-btn--change');
+  nav.classList.toggle('nav--change');
+});
